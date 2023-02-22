b4072024b7d2d51025300b79b8334e8b3fe82f81
AlinaS13 <soulofangel13@gmail.com>
2023-02-22 11:12:05 +0200

    add modl  logic

--- src/js/modal-cocktails.js
+++ src/js/modal-cocktails.js
@@ -2,6 +2,7 @@ import fetch from './fetch';
 import { getSetLS } from './localStoregeaAddRemowe';
 import { keys } from './localStoregeKeys';
 const fetchCocktailDetailsById = fetch.fetchCocktailDetailsById;
+import { createIngredientContentsMarkup } from './modal-ingredients';
 //
 //
 
@@ -53,15 +54,28 @@ function onGalleryClick(e) {
     });
     const ingredientModal = document.querySelectorAll('.JSIngridients');
     ingredientModal.forEach(item => {
-      item.addEventListener('click', e => {
+      item.addEventListener('click', async e => {
         e.preventDefault();
 
         // this is the name of ingridient
-        // e.target.dataset.name;
-
+        const data = e.target.dataset.name;
+        console.log(data);
         // here we call fetch (www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka) by name above
         // fetch()
-
+        const resp = await fetch.fetchIngredientByName(data);
+        const finalResp = resp.ingredients[0];
+        const keys = Object.keys(finalResp);
+        for (const key of keys) {
+          if (finalResp[key] === null || finalResp[key] === undefined) {
+            finalResp[key] = '';
+          }
+        }
+        console.log(finalResp);
+        const markupFF = createIngredientContentsMarkup(finalResp);
+        console.log(markupFF);
+        refs.ingridienrsModal.querySelector(
+          '.cocktail-ingredients-modal-contents'
+        ).innerHTML = markupFF;
         // parse response data and put/replace! them to modal (refs.ingridienrsModal)
         // then show the modal
         refs.ingridienrsModal.classList.remove('is-hidden');
@@ -117,7 +131,7 @@ function createCoctailInfoMarkup({
               .join('')}
         </ul>
         </div>
-        <button type="button" class="button-more modal-add" id=${idDrink} >${
+        <div class ="button-wrap"><button type="button" class="button-more modal-add" id=${idDrink} >${
     localStorageFM?.includes(idDrink) ? 'Remove' : 'Add to favorit'
-  }</button>`;
+  }</button></div>`;
 }
